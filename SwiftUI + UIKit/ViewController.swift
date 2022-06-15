c703ceb6652231b7a1b99ccc3636d5c4166a29fc
ruslan.sanarkhin <ruslan.sanarkhin@omega-r.com>
2022-06-15 10:51:40 +0300

    Initial Commit

---
+++ SwiftUI + UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUI + UIKit
+//
+//  Created by Omega on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
